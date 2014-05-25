fbef0123243fdb19ce05583f4d7c70bcfd02d7ab
Eunice Kim <ekimed@gmail.com>
2014-05-24 20:34:37 -0600

    eunice's order

--- dinner.bash
+++ dinner.bash
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 g++ stefan.cpp
+gcc eunice.c -o eunice
 
 go run zach.go
 python NWPlayer123Order.py
 ./a.out 
 brainfuck Dylan.bfck
+./eunice
+
 
 rm ./a.out
+rm ./eunice
---
+++ eunice.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("Eunice-6 Taquitos with Guacamole, Sour Cream, Cheese, Corn Salsa (on the side).");
+	return 0;
+}
